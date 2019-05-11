e9bacf6a2e8078d8bd5059898366aee84a17474e
ssingularity <1755405701@qq.com>
2019-05-11 23:34:55 +0800

    finish

--- kern/trap.c
+++ kern/trap.c
@@ -287,7 +287,7 @@ trap_dispatch(struct Trapframe *tf)
 	// Handle clock interrupts. Don't forget to acknowledge the
 	// interrupt using lapic_eoi() before calling the scheduler!
 	// LAB 4: Your code here.
-	if (tf->tf_trapno == IRQ_OFFSET + IRQ_SPURIOUS) {
+	if (tf->tf_trapno == IRQ_OFFSET + IRQ_TIMER) {
 		lapic_eoi();
 		sched_yield();
 	}
