99bc17338d7e3c071d254195d40780fc2ebb7cd5
ssingularity <1755405701@qq.com>
2019-06-07 18:55:21 +0800

    finish exercise 1

--- kern/syscall.c
+++ kern/syscall.c
@@ -380,7 +380,7 @@ static int
 sys_time_msec(void)
 {
 	// LAB 6: Your code here.
-	panic("sys_time_msec not implemented");
+	return time_msec();
 }
 
 int
@@ -447,6 +447,8 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 			return sys_ipc_try_send((envid_t) a1, (uint32_t) a2, (void *) a3, (unsigned int) a4);
 		case SYS_env_set_trapframe:
 			return sys_env_set_trapframe((envid_t) a1, (struct Trapframe *) a2);
+		case SYS_time_msec:
+			return sys_time_msec();
 		case NSYSCALLS:
 		default:
 			return -E_INVAL;
